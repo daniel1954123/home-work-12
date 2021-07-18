f842dcf4a062a12f800023754e97abeec10b445b
“daniel1954123” <m.danik20.07@gmail.com>
2021-07-18 23:34:26 +0600

    Home-Work12

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict'
+
+
+// const a = 4;
+//
+// if (a > 0 && a < 5){
+//     console.log('верно');
+// }else console.log('не верно');
+//
+//
+// const a = 10;
+// const b = 7;
+// if ((a > 2 && a < 11) || (b > 6 && b < 14)){
+//     console.log('верно');
+// }else console.log('не верно');
+
+// const a = 1;
+// const b = 7;
+//
+// if (a === 1){
+//     console.log(a + b)
+// }else console.log(a-b)
+
+
+// const num = +prompt('Введите число');
+//
+// switch (num){
+//     case 1:
+//         console.log('Winter');
+//         break;
+//     case 2:
+//         console.log('Spring');
+//         break;
+//     case 3:
+//         console.log('Summer');
+//         break;
+//     case 4:
+//         console.log('Fall');
+//         break;
+//     default:
+//         console.log('Default')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
